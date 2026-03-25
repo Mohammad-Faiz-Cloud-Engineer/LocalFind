9468820e09a7ef26bf6b4f51e98ee3a4ba1bf14d
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-03-25 13:08:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@
  * @author Mohammad Faiz
  * @repository https://github.com/Mohammad-Faiz-Cloud-Engineer/LocalFind
  * @license MIT
- * @version 4.2.1
- * @updated 2026-03-17
+ * @version 4.3.0
+ * @updated 2026-03-25
  * 
  * Configuration is loaded from config.js
  */
@@ -142,7 +142,7 @@ function renderFooter(){
       </div>
     </div>
     <div class="container footer-bottom">
-      &copy; ${new Date().getFullYear()} ${siteName}. All rights reserved. <span style="color: var(--text-disabled); margin-left: 8px;">v${CONFIG.version || '4.2.0'}</span>
+      &copy; ${new Date().getFullYear()} ${siteName}. All rights reserved. <span style="color: var(--text-disabled); margin-left: 8px;">v${CONFIG.version || '4.3.0'}</span>
       <button id="back-to-top" class="btn" aria-label="Scroll back to top">Back to top</button>
     </div>
   </footer>
