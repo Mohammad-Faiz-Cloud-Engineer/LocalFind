76989d0e015523dcfe537443f79f57d844c58d4e
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-03-31 18:03:30 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,8 +2,8 @@
  * Map Integration
  * Handles map embed for business detail pages
  * 
- * @version 4.3.1
- * @updated 2026-03-25
+ * @version 4.3.2
+ * @updated 2026-03-31
  */
 (function() {
   'use strict';
