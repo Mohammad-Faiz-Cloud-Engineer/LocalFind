dcd24020d4d735000705c57ab71b56498e6b54f8
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-03-31 23:50:25 +0530

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -2,7 +2,7 @@
  * Animations and Intersection Observer
  * Handles scroll-triggered animations and hero canvas particles
  * 
- * @version 4.3.2
+ * @version 4.3.3
  * @updated 2026-03-31
  */
 (function() {
