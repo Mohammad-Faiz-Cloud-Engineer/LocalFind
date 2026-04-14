c93f1cfdec3d06dfbedaff897f08e689a60ecc38
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-04-15 01:29:10 +0530

    Update business-detail.js

--- js/business-detail.js
+++ js/business-detail.js
@@ -2,8 +2,8 @@
  * Business Detail Page - Rendering Logic
  * Handles business detail page display with proper styling and security
  * 
- * @version 4.3.3
- * @updated 2026-03-31
+ * @version 4.3.5
+ * @updated 2026-04-15
  */
 (function () {
   'use strict';
